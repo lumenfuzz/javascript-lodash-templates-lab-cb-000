8e9be5e571ae27fb383205334fb34edc64b42096
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-06 17:25:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function createPost() {
   var pageTemplate = document.getElementById("page-template").innerHTML;
   var postTemplate = document.getElementById("post-template").innerHTML;
   //create template functions
-  var templateFn;
+  var templateFn = _.template(pageTemplate, postTemplate);
   var pageTemplateFn = _.template(pageTemplate);
   var postTemplateFn = _.template(postTemplate);
 
