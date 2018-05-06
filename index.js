698fdb519530ccd8fbb41784f9f7a9f837c0ab93
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-06 16:35:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function createPost() {
   var pageTemplate = document.getElementById("page-template").innerHTML;
   var postTemplate = document.getElementById("post-template").innerHTML;
 
+  var templateFn = _.template(postTemplate);
 }
 
 function postComment() {
