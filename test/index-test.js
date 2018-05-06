e7ccbd361c5f463d6f69a68875890ff66dd61e45
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-06 16:39:12 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -20,6 +20,7 @@ describe('index', () => {
         var spy = expect.spyOn(window._, 'template').andCallThrough()
         createPost()
         expect(spy).toHaveBeenCalledWith(document.getElementById("page-template").innerHTML)
+        expect(spy).toHaveBeenCalledWith(document.getElementById("post-template").innerHTML)
       })
     })
 
