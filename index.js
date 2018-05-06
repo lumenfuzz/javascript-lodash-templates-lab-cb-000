dc9707f64c7beadbab0405411760116a9ecd61ef
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-06 16:35:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function createPost() {
   var pageTemplate = document.getElementById("page-template").innerHTML;
   var postTemplate = document.getElementById("post-template").innerHTML;
-  
+
 }
 
 function postComment() {
