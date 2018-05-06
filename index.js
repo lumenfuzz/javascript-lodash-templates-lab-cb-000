d5d5078c91153ee4c5ed8f9dbbfeeca5adb64f54
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-06 17:19:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function createPost() {
+  var postTitle = document.getElementById("postTitle").value;
+  var postBody = document.getElementById("postBody").value;
+  var postAuthor = document.getElementById("postAuthor").value;
+
+  //create template strings
   var pageTemplate = document.getElementById("page-template").innerHTML;
   var postTemplate = document.getElementById("post-template").innerHTML;
-
+  //create template functions
   var pageTemplateFn = _.template(pageTemplate);
   var postTemplateFn = _.template(postTemplate);
+
+  //get HTMLspace for the post
+  var thePostDiv = document.getElementById("the_post");
+
+  //execute template function with JSON object for the interpolated values
+  var templateHTML = templateFn({ 'title': postTitle, 'body': postBody, 'author': postAuthor });
+
+  //append rather than replace!
+  thePostDiv.innerHTML += templateHTML;
 }
 
 function postComment() {
